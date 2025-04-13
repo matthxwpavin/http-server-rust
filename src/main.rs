81ca9906a5f572180a47b6b90f5d47527a9937d9
Pavin Wannasirilak <Matthxw@MacBook-Pro.local>
2025-04-14 03:07:35 +0700

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    ops::Index,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!!!");
-
     // Uncomment this block to pass the first stage
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -14,7 +16,27 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                _ = stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
+                let mut buffer = String::new();
+                _ = match BufReader::new(&stream).read_line(&mut buffer) {
+                    Ok(_) => {
+                        if buffer.split(" ").collect::<Vec<&str>>()[1]
+                            == "/abcdefg"
+                        {
+                            stream.write_all(
+                                "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes(),
+                            )
+                        } else {
+                            stream
+                                .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                        }
+                    }
+                    Err(error) => {
+                        println!("an error occurred: {error:?}");
+                        stream.write_all(
+                            "HTTP/1.1 500 Internal Server Error".as_bytes(),
+                        )
+                    }
+                };
             }
             Err(e) => {
                 println!("error: {}", e);
